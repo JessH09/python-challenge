c0707f5a3995f4972ad05c50c933b7403147133b
JessH09 <jesshernan0919@gmail.com>
2023-12-21 18:16:18 -0500

    add folders

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,28 @@
 #PyBank Assignment
+
+
+#analyze and calculate toal number of months included in dataset
+
+
+
+
+#analyze and calculate net total amount of Profit/loss over the entire period
+
+
+
+
+
+#calculate and analyze greatest increase in profits(date and amount) over entire period
+
+
+
+
+
+
+#calculate and analyze greatest decrease in profits (date and amt) over entire period
+
+
+
+
+
+#final script needs to print out analysis in terminal and export a text file with results
